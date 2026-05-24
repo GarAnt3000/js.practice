5261468ee1e57a3659c0b27989bffc73f3e7a2c6
harbuzov.anton <harbuzov333@gamil.com>
2026-05-24 11:46:56 +0200

    HW5

---
+++ lesson5/test3.js
@@ -0,0 +1,23 @@
+let number1 = 1;
+
+for (; number1 <= 10; number1++) {
+  console.log(number1 * 3);
+}
+
+
+const number = 5;
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`${number} x ${i} = ${number * i}`)
+}
+
+
+
+
+let x = 5;
+let y = 1;
+
+while (y <= 10) {
+  console.log(`${x} x ${y} = ${x * y}`)
+  y++;
+}
